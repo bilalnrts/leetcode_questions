ec7e46368c5ce1ef517d321795543d628efe9b39
Bilal Salih Nurtas <52494435+bilalnrts@users.noreply.github.com>
2023-06-08 21:11:08 +0300

    Time: 239 ms (5.45%), Space: 45.4 MB (5.24%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        self.myFunc(s, len(s), 0)
+    
+    def myFunc(self, s, lenght, current : int):
+        if (current == int(lenght / 2)):
+            return True
+        else:
+            s[current], s[-(current + 1)] = s[-(current + 1)], s[current]
+            return self.myFunc(s, lenght, current + 1)
